0c7b2184436a601171e540e724564e3c8796534c
Alireza Shamsi <almator5@gmail.com>
2022-06-12 23:22:15 +0430

    add model to coffe_app

--- DjangoAsh/coffehouse/coffe_app/models.py
+++ DjangoAsh/coffehouse/coffe_app/models.py
@@ -1,3 +1,18 @@
+from audioop import add
 from django.db import models
 
 # Create your models here.
+class Store(models.Model):
+    #id field is added by default, not required explicitly
+    ##id = models.AutoField(primary_key=True) #Django < 3.2
+    ##id = models.BigAutoField(primary_key=True) #Django > 3.2
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=30)
+    state = models.CharField(max_length=2)
+    ## object variable is added by default, not required explicitly
+    #objects = models.Manager()
+    def __str__(self):
+        return "%s (%s, %s)"%(self.name, self.city, self.state)
+        #also you can use python format function
+        ##return "{}({}, {})", format(self.name, self.city, self.state)
