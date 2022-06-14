e032fb7ed32ce8f34036a759642542b200dd7c73
Alireza Shamsi <almator5@gmail.com>
2022-06-14 23:01:23 +0430

    Avoid to submit a blank or null data

--- DjangoAsh/coffehouse/coffe_app/models.py
+++ DjangoAsh/coffehouse/coffe_app/models.py
@@ -1,6 +1,10 @@
 from audioop import add
 from django.db import models
 
+
+#Evoid to submit null data or blank
+def default_city():
+    return "London"
 # Create your models here.
 class Store(models.Model):
     #id field is added by default, not required explicitly
@@ -8,8 +12,8 @@ class Store(models.Model):
     ##id = models.BigAutoField(primary_key=True) #Django > 3.2
     name = models.CharField(max_length=30)
     address = models.CharField(max_length=50)
-    city = models.CharField(max_length=30)
-    state = models.CharField(max_length=2)
+    city = models.CharField(max_length=30, default=default_city)
+    state = models.CharField(max_length=2, default="UK")
     ## object variable is added by default, not required explicitly
     #objects = models.Manager()
     def __str__(self):
