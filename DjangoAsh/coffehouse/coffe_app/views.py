16c1fab0c775cbd3b4b507fba92a4bcea20533e5
Alireza Shamsi <almator5@gmail.com>
2022-06-24 23:11:25 +0430

    add save method

--- DjangoAsh/coffehouse/coffe_app/views.py
+++ DjangoAsh/coffehouse/coffe_app/views.py
@@ -1,6 +1,25 @@
+import email
+from unicodedata import name
 from django.shortcuts import render
+from models import Store
+
+#showing a message in home page
 def show_home_page(request):
     ##do something here ....
     mycontent = {'mykey': 'We love IRAN'}
     return render(request, "homepage.html", context = mycontent)
 # Create your views here.
+
+#create model store instance
+my_store = Store(name = 'AlirezaShamsi', address = 'Piccadilly St, No.72', city = 'London', state = 'UK', email = 'alireza@alirezashamsi.com')
+
+#Invoke the save() method to create/save the record
+my_store.save()
+
+#change field on instance (an update operation)
+my_store.address = 'SOHO st, No.9'
+
+#invoke the save() method to update/save the record
+##record has id regerence from prior save() call, so operation is update 
+my_store.save()
+
