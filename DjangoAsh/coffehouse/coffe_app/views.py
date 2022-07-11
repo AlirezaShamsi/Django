b0af3fe28682afa1b7f17bd54b1e8e9d8e4a7ec7
Alireza Shamsi <almator5@gmail.com>
2022-07-11 23:34:51 +0430

    add unique_together method

--- DjangoAsh/coffehouse/coffe_app/views.py
+++ DjangoAsh/coffehouse/coffe_app/views.py
@@ -91,5 +91,17 @@ ValidationError:{'address':[u'Store with this adress already exist']}
 '''
 
 
+#for unique_together
+##store_kaaj = Store(name='kaaj store', address='France ', city='Paris', state= 'Pr', email='kaaj@kaaj.com')
+#Save instance to DB
+##store_kajj.save()
 
+#create additional instance that violated unique_together rule in Meta class
+##store_kaaj2 = Store(name='kaaj store', address='Italy ', city='Rome', state= 'Ro', email='kaaj@kaaj.com')
+
+##store_kaaj2.validate_unique()
+'''
+Traceback(most recent call last):
+ValidationError:{'__all__':[u'Store with this Name and Email already exist']}
+'''
 
