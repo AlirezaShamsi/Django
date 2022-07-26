3a52ebd88b0180c7e97c1ba97bd4f6e51ada0935
Alireza Shamsi <almator5@gmail.com>
2022-07-26 23:23:42 +0430

    add extra example to inheritance class

--- DjangoAsh/coffehouse/coffe_app/models.py
+++ DjangoAsh/coffehouse/coffe_app/models.py
@@ -96,6 +96,10 @@ class Store(models.Model):
         abstract = True
     class Drink(Item):
         mili_liters = models.IntegerField()
+        #Example
+        abstract = True
+    class Student(CommonInfo):
+        home_group = models.CharField(max_length=5)
 
 
     def __str__(self):
