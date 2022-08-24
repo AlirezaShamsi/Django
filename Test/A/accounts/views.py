e7748e6f4132f3377f3f2888da1f15bfc7bfdd45
Alireza Shamsi <almator5@gmail.com>
2022-08-24 23:52:24 +0430

    add register

---
+++ Test/A/accounts/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def user_register(request):
+    return HttpResponse('User registeration page')
+
+# Create your views here.
