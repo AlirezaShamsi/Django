733234534ce146dcc29eebadd8c1c8f256590453
Alireza Shamsi <almator5@gmail.com>
2022-08-27 01:13:11 +0430

    add login logout

--- Test/A/accounts/views.py
+++ Test/A/accounts/views.py
@@ -1,9 +1,11 @@
 from ast import Pass
+import re
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 def user_register(request):
     if request.method == 'POST':
@@ -22,5 +24,32 @@ def user_register(request):
     else:
         form = UserRegistrationForm()
     return render(request, 'register.html' , {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username = username, password = password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'Login seccesfully', 'success')
+                return redirect('home')
+            else:
+                messages.error(request, 'Username or Password are wrong', 'danger')
+
+    else:
+        form = UserLoginForm()
+    return render(request, 'login.html', {'form':form})
+
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, 'Logged out successfully', 'info')
+    return redirect('home')
+
+
 
 # Create your views here.
